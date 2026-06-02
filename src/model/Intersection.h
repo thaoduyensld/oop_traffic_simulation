98afc0602bca353fa7038c2eebb4622bf3d08e36
thaoduyensld <buithaoduyen1009@gmail.com>
2026-06-02 23:10:24 +0700

    Update Intersection.h
    add incoming and outcoming roads (add and remove)

--- src/model/Intersection.h
+++ src/model/Intersection.h
@@ -6,12 +6,15 @@
 
 class Road;  
 
+// attributes
 class Intersection {
 private:
     int id;
     double x;
     double y;
-    std::vector<Road*> connectedRoads; //list of roads that connect with this intersection
+    
+    std::vector<Road*> incomingRoads; 
+    std::vector<Road*> outgoingRoads;
 
 public:
     Intersection(int id, double x = 0.0, double y = 0.0);
@@ -19,11 +22,16 @@ public:
     int getId() const;
     double getX() const;
     double getY() const;
-    std::vector<Road*> getConnectedRoads() const;
 
-    void addConnectedRoad(Road* road);
     std::vector<Road*> getIncomingRoads() const; 
     std::vector<Road*> getOutgoingRoads() const; 
+
+    //methods
+    void addIncomingRoad(Road* road);
+    void addOutgoingRoad(Road* road);
+    void removeIncomingRoad(Road* road);
+    void removeOutgoingRoad(Road* road);
+    
     std::string toString() const;  
 
     ~Intersection(); 
