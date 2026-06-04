f4397ba23262565928c225213e8a06d27214b279
Lê Tấn Lộc <letanloc207@gmail.com>
2026-06-04 17:21:14 +0700

    refactor: remove Vehicle components from class Graph

--- src/model/Graph.h
+++ src/model/Graph.h
@@ -5,37 +5,29 @@
 #include <vector>
 #include "Intersection.h"
 #include "Road.h"
-#include "Vehicle.h"
 
 class Graph {
 private:
-    // Sử dụng unordered_map để tra cứu nhanh bằng ID
     std::unordered_map<int, Intersection*> intersections;
     std::unordered_map<int, Road*> roads;
-    std::unordered_map<int, Vehicle*> vehicles;
 
 public:
     Graph();
     ~Graph();
 
-    // --- Graph Operations ---
+    // --- Intersection Operations ---
     void addIntersection(Intersection* intersection);
     void removeIntersection(int id);
+    Intersection* getIntersection(int id) const;
     
+    // --- Road Operations ---
     void addRoad(Road* road);
     void removeRoad(int id);
+    Road* getRoad(int id) const;
     
+    // --- Graph Functions ---
     void updateRoadCondition(int roadId, double congestionLevel, bool blocked);
     std::vector<Road*> getConnectedRoads(int intersectionId) const;
-
-    // --- Vehicle Operations ---
-    void addVehicle(Vehicle* vehicle);
-    void removeVehicle(int id);
-
-    // --- Getters ---
-    Intersection* getIntersection(int id) const;
-    Road* getRoad(int id) const;
-    Vehicle* getVehicle(int id) const;
 
     // --- Utility ---
     void clearGraph();
